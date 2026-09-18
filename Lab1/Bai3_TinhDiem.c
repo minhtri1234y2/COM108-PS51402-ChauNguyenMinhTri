2a67590a651d056ffdc5faea60d013586e5859de
minhtri1234y2 <chautri355@gmail.com>
2026-09-19 06:01:43 +0700

    Bai4

--- Lab1/Bai3_TinhDiem.c
+++ Lab1/Bai3_TinhDiem.c
@@ -4,9 +4,9 @@ int main() {
     char mssv[] = "12345678";
     char hoTen[] = "Nguyen Van A";
 
-    float toan = 8.5;
-    float ly = 7.5;
-    float hoa = 8.0;
+    float toan = 9.0;
+    float ly = 8.0;
+    float hoa = 9.0;
 
     float diemTrungBinh = (toan * 2 + ly + hoa) / (float)4;
 
