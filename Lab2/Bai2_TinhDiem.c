2659226a6ebe4b7dc0bfc3cbc8cfdd07ab3eaa71
minhtri1234y2 <chautri355@gmail.com>
2026-09-22 11:15:18 +0700

    Bai

--- Lab2/Bai2_TinhDiem.c
+++ Lab2/Bai2_TinhDiem.c
@@ -3,22 +3,13 @@
 int main() {
     float Toan, Ly, Hoa;
     float DiemTrungBinh;
-
-    // Nhap diem
     printf("Nhap diem Toan: ");
     scanf("%f", &Toan);
-
     printf("Nhap diem Ly: ");
     scanf("%f", &Ly);
-
     printf("Nhap diem Hoa: ");
     scanf("%f", &Hoa);
-
-    // Tinh diem trung binh
     DiemTrungBinh = (Toan * 3 + Ly * 2 + Hoa * 1) / 6;
-
-    // Xuat ket qua
     printf("Diem trung binh: %.2f\n", DiemTrungBinh);
-
     return 0;
 }
